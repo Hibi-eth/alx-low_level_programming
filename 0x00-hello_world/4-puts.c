00b4b055d25f26051c592e59e47e355d3d94ea66
Hibi-eth <yenaehibi@gmail.com>
2022-07-01 13:37:46 -0700

    Task-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * Description: main - prints the pharse
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+Return (0)
+}
