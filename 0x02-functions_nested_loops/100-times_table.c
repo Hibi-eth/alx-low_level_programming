3f372d7e45ca0748e10e12896aa6d306434189d4
Hibi-eth <yenaehibi@gmail.com>
2022-07-05 15:53:31 -0700

    Task-12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * prints_times_table - prints the n times table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
 void print_times_table(int n)
@@ -43,5 +43,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-}
-						
+}						
