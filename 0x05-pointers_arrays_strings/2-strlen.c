52dd0d1e278eafdff8fdfb4728edce6c6e328396
Hibi-eth <yenaehibi@gmail.com>
2022-07-11 14:03:06 -0700

    Task-2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
-/**_strlen - returns the length of a string
+/**
+ * _strlen - returns the length of a string
+ *
  * @s: string to evaluate
  * Return: the length of the string
  */
