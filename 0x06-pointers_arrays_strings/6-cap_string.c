a321d2cf6afc14658b2a09e904647ba9bb7a013d
Hibi-eth <yenaehibi@gmail.com>
2022-07-14 12:17:48 -0700

    Task-6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes every word of a string
+ * cap_string - capitalizes everey word of a string
  * @s: string to modify
  *
  * Return: the resulting string
@@ -10,8 +10,7 @@ char *cap_string(char *s)
 {
 	int i, j;
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!',
-		'!', '?', '"', '(', ')', '{', '}'};
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
